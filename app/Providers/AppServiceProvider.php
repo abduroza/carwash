d42ce22400935b41fff451f8542423ea752c936a
Muhammad Abdurrozaq <abduroza@gmail.com>
2020-08-13 11:04:01 +0700

    edit AppServiceProvider at boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //to prevent erros while deploy to heroku. SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+        Schema::defaultStringLength(191);
     }
 }
