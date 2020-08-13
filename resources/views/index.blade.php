19a828a45506ec640e14b0749662f08bc2b0cf94
Muhammad Abdurrozaq <abduroza@gmail.com>
2020-08-13 13:48:10 +0700

    change to secured_asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,12 +8,12 @@
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 4.5.0 -->
-        <link rel="stylesheet" href="{{ asset('bootstrap-4.5.0/dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ secured_asset('bootstrap-4.5.0/dist/css/bootstrap.min.css') }}">
         {{-- custom css --}}
-        <link rel="stylesheet" href="{{ asset('mycss/style.css') }}">
+        <link rel="stylesheet" href="{{ secured_asset('mycss/style.css') }}">
         <!-- Font Awesome 4.7.0 -->
-        <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('linearicon/linearicon.min.css') }}">
+        <link rel="stylesheet" href="{{ secured_asset('font-awesome/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ secured_asset('linearicon/linearicon.min.css') }}">
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
@@ -24,11 +24,11 @@
         </div>
 
         <!-- jQuery 3.5.1 -->
-        <script src="{{ asset('jquery/jquery-3.5.1.min.js') }}"></script>
+        <script src="{{ secured_asset('jquery/jquery-3.5.1.min.js') }}"></script>
         <!-- Bootstrap  -->
-        <script src="{{ asset('bootstrap-4.5.0/dist/js/bootstrap.min.js') }}"></script>
+        <script src="{{ secured_asset('bootstrap-4.5.0/dist/js/bootstrap.min.js') }}"></script>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ secured_asset('js/app.js') }}"></script>
     </body>
 </html>
 <!-- tag <app></app>, dimana tag tersebut merupakan custom tag yang dibuat dari component Vue.js dan pastikan tag div yang mengapitnya memiliki id appku. Adapun tag lainnya hanya me-load file js dan css. -->
